0c445ff6e0ff3ebcb21be1c6058c2776432d5f96
沧浪 <757465172@qq.com>
2022-11-30 12:19:10 +0800

    feat: instancing - 小行星带（未用instancing）

--- chapters/advanced_opengl/advanced_instancing.cpp
+++ chapters/advanced_opengl/advanced_instancing.cpp
@@ -24,7 +24,7 @@ const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
 // camera attributes
-Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+Camera camera(glm::vec3(0.0f, 0.0f, 55.0f));
 float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
 bool firstMouse = true;
@@ -76,62 +76,42 @@ int main() {
 
     glEnable(GL_DEPTH_TEST);
 
-    float quadVertices[] = {
-            // positions     // colors
-            -0.05f, 0.05f, 1.0f, 0.0f, 0.0f,
-            0.05f, -0.05f, 0.0f, 1.0f, 0.0f,
-            -0.05f, -0.05f, 0.0f, 0.0f, 1.0f,
-
-            -0.05f, 0.05f, 1.0f, 0.0f, 0.0f,
-            0.05f, -0.05f, 0.0f, 1.0f, 0.0f,
-            0.05f, 0.05f, 0.0f, 1.0f, 1.0f
-    };
-
-    glm::vec2 translations[100];
-    int index = 0;
-    float offset = 0.1f;
-    for (int y = -10; y < 10; y += 2) {
-        for (int x = -10; x < 10; x += 2) {
-            glm::vec2 translation;
-            translation.x = (float) x / 10.0f + offset;
-            translation.y = (float) y / 10.0f + offset;
-            translations[index++] = translation;
-        }
-    }
-
-
     // shader declaration
-    Shader shader("../chapters/advanced_opengl/shaders_instancing/instancing.vert",
-                  "../chapters/advanced_opengl/shaders_instancing/instancing.frag");
+    Shader shader("../chapters/advanced_opengl/shaders_instancing/object.vert",
+                  "../chapters/advanced_opengl/shaders_instancing/object.frag");
 
+    Model planet(FileSystem::getPath("resources/planet/planet.obj"));
+    Model rock(FileSystem::getPath("resources/rock/rock.obj"));
 
-    Model ourModel(FileSystem::getPath("resources/tea_set_01_1k.obj"));
+    unsigned int amount = 1000;
+    glm::mat4 *modelMatrices;
+    modelMatrices = new glm::mat4[amount];
+    srand(glfwGetTime());
+    float radius = 50.0;
+    float offset = 2.5f;
 
-    unsigned int instanceVBO;
-    glGenBuffers(1, &instanceVBO);
-    glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec2) * 100, &translations[0], GL_STATIC_DRAW);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    for (unsigned int i = 0; i < amount; i++) {
+        glm::mat4 model = glm::mat4(1.0f);
+        // 1. translation: displace along circle with 'radius' in range [-offset, offset]
+        float angle = (float) i / (float) amount * 360.0f;
+        float displacement = (rand() % (int) (2 * offset * 100)) / 100.0f - offset;
+        float x = sin(angle) * radius + displacement;
+        displacement = (rand() % (int) (2 * offset * 100)) / 100.0f - offset;
+        float y = displacement * 0.4f; // keep height of field smaller compared to width of x and z
+        displacement = (rand() % (int) (2 * offset * 100)) / 100.0f - offset;
+        float z = cos(angle) * radius + displacement;
+        model = glm::translate(model, glm::vec3(x, y, z));
 
-    unsigned int quadVAO, quadVBO;
-    glGenVertexArrays(1, &quadVAO);
-    glGenBuffers(1, &quadVBO);
-    glBindVertexArray(quadVAO);
-    glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *) 0);
-    glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *) (2 * sizeof(float)));
-    glEnableVertexAttribArray(2);
-    glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void *) 0);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glVertexAttribDivisor(2, 1);
+        // 2. scale: scale between 0.05 and 0.25f
+        float scale = (rand() % 20) / 100.0f + 0.05;
+        model = glm::scale(model, glm::vec3(scale));
 
-    shader.use();
-    for (unsigned int i = 0; i < 100; i++) {
-        shader.setVec2(("offsets[" + std::to_string(i) + "]"), translations[i]);
+        // 3. rotation: add random rotation around a (semi)randomly picked rotation axis vector
+        float rotAngle = (rand() % 360);
+        model = glm::rotate(model, rotAngle, glm::vec3(0.4f, 0.6f, 0.8f));
+
+        // 4. now add to list of matrices
+        modelMatrices[i] = model;
     }
 
 
@@ -155,13 +135,24 @@ int main() {
         glClearColor(0.1f, 0.1f, 0.1f, 0.1f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        // be sure to activate shader when setting uniforms/drawing objects
+        // draw planet
         shader.use();
+        glm::mat4 view = camera.GetViewMatrix();
+        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float) SCR_WIDTH / (float) SCR_HEIGHT, 0.1f,
+                                                100.0f);
+        glm::mat4 model = glm::mat4(1.0f);
+        shader.setMat4("view", view);
+        shader.setMat4("projection", projection);
+        model = glm::translate(model, glm::vec3(0.0f, -3.0f, 0.0f));
+        model = glm::scale(model, glm::vec3(4.0f, 4.0f, 4.0f));
+        shader.setMat4("model", model);
+        planet.Draw(shader);
 
-        glBindVertexArray(quadVAO);
-        glDrawArraysInstanced(GL_TRIANGLES, 0, 6, 100);
-
-
+// draw meteorites
+        for (unsigned int i = 0; i < amount; i++) {
+            shader.setMat4("model", modelMatrices[i]);
+            rock.Draw(shader);
+        }
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
